51de2d58337d5bf37bcaa992cd3173bb639515b0
Dmitriy Sintsov <questpc256@gmail.com>
2018-12-17 17:31:16 +0300

    Separate parse_rule() function.

--- compare.py
+++ compare.py
@@ -12,10 +12,40 @@ def parse_css_file(fname):
         css_bytes=content,
         protocol_encoding='utf-8',
         environment_encoding='utf-8',
-    skip_comments=True,
-    skip_whitespace=True
+        skip_comments=True,
+        skip_whitespace=True
     )
     return rules
+
+
+def parse_rule(rule):
+    # print('type of rule: {}'.format(rule.type))
+    is_content_token = False
+    for token in rule.content:
+        if not isinstance(token, ast.WhitespaceToken):
+            # print(token.value)
+            if isinstance(token, ast.IdentToken) and token.value == 'content':
+                is_content_token = True
+            if isinstance(token, ast.StringToken) and is_content_token:
+                for pt in rule.prelude:
+                    if isinstance(pt, ast.IdentToken):
+                        content_token = token
+                        font_css_token = pt
+                        return font_css_token, content_token
+    return None, None
+
+
+def get_font_map(rules):
+    font_map = {}
+    for rule in rules:
+        font_css_token, content_token = parse_rule(rule)
+        if font_css_token is not None:
+            # Convert char to entity. Commented out.
+            # if not content_token.value.startswith('\\u'):
+            #     content = '\\u{:04x}'.format(ord(token.value))
+            # font_family, glyph_name = font_css.split('-', 1)
+            font_map[font_css_token.value] = rule
+    return font_map
 
 
 def parse_gi_to_fa_map(fname):
@@ -35,49 +65,19 @@ def parse_gi_to_fa_map(fname):
     f.close()
     return gi_to_fa_map
 
+
 def generate_css(css_fname, gi_to_fa_map, font_map_fa):
     with open(css_fname, 'w+') as f:
         for glyph_name, fa_name in gi_to_fa_map.items():
             if fa_name in font_map_fa:
                 rule = font_map_fa[fa_name]
-                for token in rule.content:
-                    if not isinstance(token, ast.WhitespaceToken):
-                        # print(token.value)
-                        if isinstance(token, ast.IdentToken) and token.value == 'content':
-                            is_content_token = True
-                        if isinstance(token, ast.StringToken) and is_content_token:
-                            for pt in rule.prelude:
-                                if isinstance(pt, ast.IdentToken):
-                                    pt.value = glyph_name
-                                    print(rule.serialize(), file=f)
-                                    print('/**/', file=f)
-                                    break
+                font_css_token, content_token = parse_rule(rule)
+                if font_css_token is not None:
+                    font_css_token.value = glyph_name
+                    print(rule.serialize(), file=f)
             else:
                 print('Warning: mapping fa name "{}" is not found for glyphicon: "{}"'.format(glyph_name, fa_name))
     f.close()
-
-def get_font_map(rules):
-    font_map = {}
-    for rule in rules:
-        # print('type of rule: {}'.format(rule.type))
-        is_content_token = False
-        for token in rule.content:
-            if not isinstance(token, ast.WhitespaceToken):
-                # print(token.value)
-                if isinstance(token, ast.IdentToken) and token.value == 'content':
-                    is_content_token = True
-                if isinstance(token, ast.StringToken) and is_content_token:
-                    for pt in rule.prelude:
-                        if isinstance(pt, ast.IdentToken):
-                            content = token.value
-                            font_css = pt.value
-                            # Convert char to entity. Commented out.
-                            # if not content.startswith('\\u'):
-                            #     content = '\\u{:04x}'.format(ord(token.value))
-                            # font_family, glyph_name = font_css.split('-', 1)
-                            font_map[font_css] = rule
-                            break
-    return font_map
 
 
 rules = parse_css_file('glyphicon.css')
