90170666029144e15885b6a9fca088a890e3b2db
Dmitriy Sintsov <questpc256@gmail.com>
2018-12-17 17:19:25 +0300

    Generation of map.css works partially.

--- compare.py
+++ compare.py
@@ -1,7 +1,9 @@
+import re
+
 from tinycss2 import parse_stylesheet_bytes, ast
 
 
-def parse_file(fname):
+def parse_css_file(fname):
     with open(fname, 'r') as f:
         content = f.read()
     f.close()
@@ -15,6 +17,44 @@ def parse_file(fname):
     )
     return rules
 
+
+def parse_gi_to_fa_map(fname):
+    gi_to_fa_map = {}
+    with open(fname, 'r') as f:
+        source_glyphicon = None
+        for line in f:
+            result = re.findall(r"\s*&\.(.+?)\s*\{", line, re.UNICODE)
+            if len(result) > 0:
+                source_glyphicon = result[0]
+            else:
+                result = re.findall(r"\s*@extend\s*\.(.*?);\s*", line, re.UNICODE)
+                if len(result) > 0:
+                    if source_glyphicon != None:
+                        gi_to_fa_map[source_glyphicon] = result[0]
+                        source_glyphicon = None
+    f.close()
+    return gi_to_fa_map
+
+def generate_css(css_fname, gi_to_fa_map, font_map_fa):
+    with open(css_fname, 'w+') as f:
+        for glyph_name, fa_name in gi_to_fa_map.items():
+            if fa_name in font_map_fa:
+                rule = font_map_fa[fa_name]
+                for token in rule.content:
+                    if not isinstance(token, ast.WhitespaceToken):
+                        # print(token.value)
+                        if isinstance(token, ast.IdentToken) and token.value == 'content':
+                            is_content_token = True
+                        if isinstance(token, ast.StringToken) and is_content_token:
+                            for pt in rule.prelude:
+                                if isinstance(pt, ast.IdentToken):
+                                    pt.value = glyph_name
+                                    print(rule.serialize(), file=f)
+                                    print('/**/', file=f)
+                                    break
+            else:
+                print('Warning: mapping fa name "{}" is not found for glyphicon: "{}"'.format(glyph_name, fa_name))
+    f.close()
 
 def get_font_map(rules):
     font_map = {}
@@ -31,41 +71,24 @@ def get_font_map(rules):
                         if isinstance(pt, ast.IdentToken):
                             content = token.value
                             font_css = pt.value
-                            if not content.startswith('\\u'):
-                                content = '\\u{:04x}'.format(ord(token.value))
-                            font_map[content] = font_css
+                            # Convert char to entity. Commented out.
+                            # if not content.startswith('\\u'):
+                            #     content = '\\u{:04x}'.format(ord(token.value))
+                            # font_family, glyph_name = font_css.split('-', 1)
+                            font_map[font_css] = rule
                             break
     return font_map
 
 
-def add_to_content_map(content_map, font_map):
-    for content, font_css in font_map.items():
-        font_family, glyph_name = font_css.split('-', 1)
-        if content not in content_map:
-            content_map[content] = {}
-        content_map[content][font_family] = glyph_name
+rules = parse_css_file('glyphicon.css')
+font_map_glyphicon = get_font_map(rules)
 
+rules = parse_css_file('font-awesome-src.css')
+font_map_fa = get_font_map(rules)
 
-def add_to_name_map(name_map, font_map):
-    for content, font_css in font_map.items():
-        font_family, glyph_name = font_css.split('-', 1)
-        if glyph_name not in name_map:
-            name_map[glyph_name] = {}
-        name_map[glyph_name][font_family] = content
+# https://gist.githubusercontent.com/blowsie/15f8fe303383e361958bd53ecb7294f9/raw/89495bbd533e7e05cc0d551557b4fbf882ed9369/glyphicon_font-awesome_convert.scss
+gi_to_fa_map = parse_gi_to_fa_map('glyphicon_font-awesome_convert.scss')
 
-
-rules = parse_file('glyphicon.css')
-font_map1 = get_font_map(rules)
-
-rules = parse_file('font-awesome-src.css')
-font_map2 = get_font_map(rules)
-
-content_map = {}
-add_to_content_map(content_map, font_map1)
-add_to_content_map(content_map, font_map2)
-
-name_map = {}
-add_to_name_map(name_map, font_map1)
-add_to_name_map(name_map, font_map2)
+generate_css('map.css', gi_to_fa_map, font_map_fa)
 
 print('done')
